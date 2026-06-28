0db6f075165d550543e42cace0373b6134110eb1
Rory Fewell <roryf@oddmatics.uk>
2026-06-28 21:46:27 +0100

    [#590] Drawing large icon label in list view

--- shared/comctl/src/lstvwctl.c
+++ shared/comctl/src/lstvwctl.c
@@ -6,6 +6,10 @@
 
 #include "../public/lstvwctl.h"
 
+#define LABEL_TEXT_SHADOW_INTENSITY 3
+#define LABEL_TEXT_SHADOW_OFFSET    (LABEL_TEXT_SHADOW_RADIUS * 2)
+#define LABEL_TEXT_SHADOW_RADIUS    2
+
 #define HITBOX_LARGE_ICON 32
 
 //
@@ -13,9 +17,16 @@
 //
 typedef struct _WinTCCtlListViewIcon
 {
+    // Basic state
+    //
     GdkPixbuf* icon;
+    gchar*     text;
     gint       x;
     gint       y;
+
+    // Drawing cache
+    //
+    cairo_surface_t* surface_text_shadow;
 } WinTCCtlListViewIcon;
 
 //
@@ -28,7 +39,12 @@ static gboolean wintc_ctl_list_view_draw(
 
 static void wintc_ctl_list_view_create_large_icon(
     WinTCCtlListView* list_view,
-    const gchar*      icon_name
+    const gchar*      icon_name,
+    const gchar*      text
+);
+static void wintc_ctl_list_view_update_icon_text_shadow(
+    WinTCCtlListView*     list_view,
+    WinTCCtlListViewIcon* icon
 );
 
 static gboolean on_list_view_button_press_event(
@@ -116,7 +132,7 @@ static void wintc_ctl_list_view_init(
         NULL
     );
 
-    wintc_ctl_list_view_create_large_icon(self, "folder");
+    wintc_ctl_list_view_create_large_icon(self, "folder", "My Stuff");
 }
 
 //
@@ -141,6 +157,8 @@ static gboolean wintc_ctl_list_view_draw(
         WinTCCtlListViewIcon* large_icon =
             (WinTCCtlListViewIcon*) iter->data;
 
+        // Icon itself
+        //
         cairo_save(cr);
         cairo_rectangle(
             cr,
@@ -158,6 +176,39 @@ static gboolean wintc_ctl_list_view_draw(
         );
         cairo_paint(cr);
         cairo_restore(cr);
+
+        // Icon label
+        //
+        PangoContext* ctx    = gtk_widget_get_pango_context(widget);
+        PangoLayout*  layout = pango_layout_new(ctx);
+
+        pango_layout_set_text(layout, large_icon->text, -1);
+
+        cairo_save(cr);
+        cairo_set_source_rgba(cr, 0.0f, 0.0f, 0.0f, 1.0f);
+
+        for (gint i = 0; i < LABEL_TEXT_SHADOW_INTENSITY; i++)
+        {
+            cairo_mask_surface(
+                cr,
+                large_icon->surface_text_shadow,
+                (gdouble) large_icon->x - LABEL_TEXT_SHADOW_OFFSET,
+                (gdouble) large_icon->y + 49 - LABEL_TEXT_SHADOW_OFFSET
+            );
+        }
+
+        cairo_restore(cr);
+
+        cairo_set_source_rgb(cr, 1.0f, 1.0f, 1.0f);
+        cairo_move_to(
+            cr,
+            (gdouble) large_icon->x,
+            (gdouble) large_icon->y + 48
+        );
+
+        pango_cairo_show_layout(cr, layout);
+
+        g_object_unref(layout);
     }
 
     return FALSE;
@@ -181,13 +232,15 @@ GtkWidget* wintc_ctl_list_view_new(void)
 //
 static void wintc_ctl_list_view_create_large_icon(
     WinTCCtlListView* list_view,
-    const gchar*      icon_name
+    const gchar*      icon_name,
+    const gchar*      text
 )
 {
     WinTCCtlListViewIcon* large_icon = g_new(WinTCCtlListViewIcon, 1);
 
-    large_icon->x = 0;
-    large_icon->y = 0;
+    large_icon->text = g_strdup(text);
+    large_icon->x    = 0;
+    large_icon->y    = 0;
 
     large_icon->icon =
         gtk_icon_theme_load_icon(
@@ -201,7 +254,129 @@ static void wintc_ctl_list_view_create_large_icon(
     list_view->list_icons =
         g_list_append(list_view->list_icons, large_icon);
 
+    large_icon->surface_text_shadow = NULL;
+    wintc_ctl_list_view_update_icon_text_shadow(list_view, large_icon);
+
     gtk_widget_queue_draw(GTK_WIDGET(list_view));
+}
+
+static void wintc_ctl_list_view_update_icon_text_shadow(
+    WinTCCtlListView*     list_view,
+    WinTCCtlListViewIcon* icon
+)
+{
+    PangoContext* ctx = gtk_widget_get_pango_context(GTK_WIDGET(list_view));
+
+    if (icon->surface_text_shadow)
+    {
+        cairo_surface_destroy(icon->surface_text_shadow);
+    }
+
+    // Work out space required for the shadow
+    //
+    PangoRectangle extents;
+    PangoLayout*   layout = pango_layout_new(ctx);
+
+    pango_layout_set_text(layout, icon->text, -1);
+
+    pango_layout_get_pixel_extents(
+        layout,
+        &extents,
+        NULL
+    );
+
+    // Render to a backing image surface
+    //
+    cairo_t* cr;
+    gint     surface_height;
+    gint     surface_stride;
+    gint     surface_width;
+
+    icon->surface_text_shadow =
+        cairo_image_surface_create(
+            CAIRO_FORMAT_ARGB32,
+            extents.width  + (LABEL_TEXT_SHADOW_RADIUS * 4),
+            extents.height + (LABEL_TEXT_SHADOW_RADIUS * 4)
+        );
+
+    surface_height = cairo_image_surface_get_height(icon->surface_text_shadow);
+    surface_stride = cairo_image_surface_get_stride(icon->surface_text_shadow);
+    surface_width  = cairo_image_surface_get_width(icon->surface_text_shadow);
+
+    cr = cairo_create(icon->surface_text_shadow);
+
+    cairo_move_to(cr, LABEL_TEXT_SHADOW_RADIUS * 2, LABEL_TEXT_SHADOW_RADIUS * 2);
+    cairo_set_source_rgba(cr, 0.0f, 0.0f, 0.0f, 1.0f);
+
+    pango_cairo_show_layout(cr, layout);
+
+    cairo_surface_flush(icon->surface_text_shadow);
+
+    // Perform the blur
+    //
+    unsigned char* buf = cairo_image_surface_get_data(icon->surface_text_shadow);
+    unsigned char* tmp = g_malloc(surface_stride * surface_height);
+
+    for (gint y = 0; y < surface_height; y++) // Horz pass
+    {
+        unsigned char* src = buf + (y * surface_stride);
+        unsigned char* dst = tmp + (y * surface_stride);
+
+        for (gint x = 0; x < surface_width; x++)
+        {
+            gint len = 0;
+            gint sum = 0;
+
+            for (
+                gint offset  = -LABEL_TEXT_SHADOW_RADIUS;
+                offset      <=  LABEL_TEXT_SHADOW_RADIUS;
+                offset++
+            )
+            {
+                gint dx = x + offset;
+
+                if (dx >= 0 && dx < surface_width)
+                {
+                    sum += src[dx * 4 + 3];
+                    len++;
+                }
+            }
+
+            dst[x * 4 + 3] = sum / len;
+        }
+    }
+
+    for (gint x = 0; x < surface_width; x++) // Vert pass
+    {
+        for (gint y = 0; y < surface_height; y++)
+        {
+            gint len = 0;
+            gint sum = 0;
+
+            for (
+                gint offset = -LABEL_TEXT_SHADOW_RADIUS;
+                offset      <  LABEL_TEXT_SHADOW_RADIUS;
+                offset++
+            )
+            {
+                gint dy = y + offset;
+
+                if (dy >= 0 && dy < surface_height)
+                {
+                    sum += tmp[(dy * surface_stride) + (x * 4) + 3];
+                    len++;
+                }
+            }
+
+            buf[(y * surface_stride) + (x * 4) + 3] = sum / len;
+        }
+    }
+
+    g_free(tmp);
+    cairo_destroy(cr);
+    g_object_unref(layout);
+
+    cairo_surface_mark_dirty(icon->surface_text_shadow);
 }
 
 //
